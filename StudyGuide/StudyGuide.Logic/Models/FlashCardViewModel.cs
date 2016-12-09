bfa38d1c5a8d4be0a0b5512dabc7ff6830c5385d
Olga Polyakova <polykova.olga98@gmail.com>
2016-12-10 02:07:50 +0300

    Added several models to Logic

---
+++ StudyGuide/StudyGuide.Logic/Models/FlashCardViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyGuide.Logic.Models
+{
+    public class FlashCardViewModel
+    {
+        public string Term { get; set; }
+        public string Definition { get; set; }
+        public int Level { get; set; }
+        public TimeSpan RepeatInterval { get; set; }
+    }
+}
---
+++ StudyGuide/StudyGuide.Logic/Models/ScheduleViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyGuide.Logic.Models
+{
+    public class ScheduleViewModel
+    {
+        public string Subject { get; set; }
+        public string WorkType { get; set; }
+        public DateTime Deadline { get; set; }
+    }
+}
