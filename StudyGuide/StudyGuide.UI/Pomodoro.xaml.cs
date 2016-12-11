7ef7339b32cb1ffd567628cb7ba9030e78f333ab
Анастасия Гуляева <strelez2411@gmail.com>
2016-12-11 17:01:26 +0300

    Added Pomodoro window

---
+++ StudyGuide/StudyGuide.UI/Pomodoro.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace StudyGuide.UI
+{
+    /// <summary>
+    /// Логика взаимодействия для Pomodoro.xaml
+    /// </summary>
+    public partial class Pomodoro : Window
+    {
+        public Pomodoro()
+        {
+            InitializeComponent();
+        }
+        void ToRest()
+        {
+            workorrestTextBlock.Text = "Rest!:)";
+            Background = new SolidColorBrush(Color.FromRgb(0, 128, 0));
+            doneButton.Background = new SolidColorBrush(Color.FromRgb(0, 128, 0));
+        }
+        void ToWork()
+        {
+            workorrestTextBlock.Text = "Work:)";
+            Background = new SolidColorBrush(Color.FromRgb(139, 0, 0));
+            doneButton.Background = new SolidColorBrush(Color.FromRgb(139, 0, 0));
+        }
+
+        private void doneButton_Click(object sender, RoutedEventArgs e)
+        {
+            // to be edited... 
+        }
+    }
+}
