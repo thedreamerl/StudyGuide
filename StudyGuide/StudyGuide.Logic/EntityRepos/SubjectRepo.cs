fa46c5528a8853a9474101fc2d5b1c0c50d43716
Olga Polyakova <polykova.olga98@gmail.com>
2016-12-11 12:11:11 +0300

    Added Subject and WorkType repositories

---
+++ StudyGuide/StudyGuide.Logic/EntityRepos/SubjectRepo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StudyGuide.DataBase.DBO;
+using StudyGuide.DataBase;
+
+namespace StudyGuide.Logic.EntityRepos
+{
+    public class SubjectRepo
+    {
+        public void AddNew(string el)
+        {
+            using (var c = new Context())
+            {
+                if (c.Subjects.FirstOrDefault(x => x.Name == el) != null)
+                    throw new ArgumentException("This subject does already exist");
+                c.Subjects.Add(new Subject { Name = el });
+                c.SaveChanges();
+            }
+        }
+
+        public IQueryable<string> ShowAll()
+        {
+            using (var c = new Context())
+            {
+                var result = from f in c.Subjects
+                             select f.Name;
+                return result;
+            }
+        }
+    }
+}
---
+++ StudyGuide/StudyGuide.Logic/EntityRepos/WorkTypeRepo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StudyGuide.DataBase.DBO;
+using StudyGuide.DataBase;
+
+namespace StudyGuide.Logic.EntityRepos
+{
+    class WorkTypeRepo
+    {
+        public void AddNew(string el)
+        {
+            using (var c = new Context())
+            {
+                if (c.WorkType.FirstOrDefault(x => x.Name == el) != null)
+                    throw new ArgumentException("This work type does already exist");
+                c.WorkType.Add(new WorkType { Name = el });
+                c.SaveChanges();
+            }
+        }
+
+        public IQueryable<string> ShowAll()
+        {
+            using (var c = new Context())
+            {
+                var result = from w in c.WorkType
+                             select w.Name;
+                return result;
+            }
+        }
+    }
+}
