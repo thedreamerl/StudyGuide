b8e7fc338c7ed18352a95ca33f7131631d81b8b4
Анастасия Космылина <anasteisha-1997@mail.ru>
2016-12-17 11:10:13 +0300

    Edited logic for Flash Cards. Added LevelUp and Delete methods to FlashCardsRepo

--- StudyGuide/StudyGuide.Logic/EntityRepos/FlashCardsRepo.cs
+++ StudyGuide/StudyGuide.Logic/EntityRepos/FlashCardsRepo.cs
@@ -11,9 +11,10 @@ namespace StudyGuide.Logic.EntityRepos
 {
     public class FlashCardsRepo
     {
+        public event Action<string> ShowMessgae;
         public void AddFlashCard(FlashCardViewModel card, ScheduleViewModel schedule)
         {
-            using (var c = new Context())
+           using (var c = new Context())
             {
                 if (c.Flashards.FirstOrDefault(x => x.Term == card.Term && x.Definition == card.Definition && x.ScheduleID.SubjectID.Name == schedule.Subject && x.ScheduleID.WorkTypeID.Name == schedule.WorkType) != null)
                     throw new ArgumentException("Similar card has been already added");
@@ -25,6 +26,31 @@ namespace StudyGuide.Logic.EntityRepos
                     ScheduleID = c.Schedule.First(x => x.SubjectID.Name == schedule.Subject && x.WorkTypeID.Name == schedule.WorkType)
                 });
                 c.SaveChanges();
+            }
+        }
+        public void LevelUp(FlashCardViewModel card,ScheduleViewModel schedule)
+        {
+            using (var c = new Context())
+            {
+                c.Flashards.FirstOrDefault(x => x.Term == card.Term && x.Definition == card.Definition && x.ScheduleID.SubjectID.Name == schedule.Subject && x.ScheduleID.WorkTypeID.Name == schedule.WorkType).Level += 1;
+                c.SaveChanges();
+                 if (c.Flashards.FirstOrDefault(x => x.Term == card.Term && x.Definition == card.Definition && x.ScheduleID.SubjectID.Name == schedule.Subject && x.ScheduleID.WorkTypeID.Name == schedule.WorkType).Level == 4)
+                {
+                    ShowMessgae?.Invoke("Congrats! You've learned this card!");
+                    DeleteCard();
+                }
+                    
+            }
+            
+        }
+        public void DeleteCard()
+        {
+            using (var c = new Context())
+            {
+                var result = (from s in c.Flashards
+                              where s.Level == 4
+                              select s).ToList();
+                c.Flashards.RemoveRange(result);
             }
         }
     }
