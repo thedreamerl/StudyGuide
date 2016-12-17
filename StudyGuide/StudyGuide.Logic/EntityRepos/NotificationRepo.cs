2473479a86fc54286e5ed0a51f6ae98847a6580c
Анастасия Космылина <anasteisha-1997@mail.ru>
2016-12-17 14:49:00 +0300

    Added NotificationRepo

---
+++ StudyGuide/StudyGuide.Logic/EntityRepos/NotificationRepo.cs
@@ -0,0 +1,26 @@
+﻿using StudyGuide.Logic.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyGuide.Logic.EntityRepos
+{
+    class NotificationRepo
+    {
+
+        public static event Action<StudyPlanViewModel> Notify;
+        public static void CheckStudyPlans()
+        {
+            var studyPlan = Factory.Default.GetStudyPlanRepo().GetStudyPlanToDo();
+            if (studyPlan != null)
+            {
+                foreach (var s in studyPlan)
+                {
+                    Notify?.Invoke(s);
+                }
+            }
+        }
+    }
+}
